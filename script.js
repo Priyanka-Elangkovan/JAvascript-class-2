8140a7843d3db70f957bb0ea8c90093d1c701750
Priyanka <priyaanka94@gmail.com>
2024-10-18 17:48:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log(12)
+
+console.log(typeof 12)
+
+var abs = "Hard"
+console.log(typeof abs)
+
+console.log(`Your abs looks very much ${abs}`) 
+
+
+const data = prompt("enter data", Number)
+console.log(`The value enterd will be ${data} and the data type is ` + typeof(data))
+
+/*
+const data = Number(prompt("enter data"))
+console.log(`The value enterd will be ${data} and the data type is ` + typeof(data))
+*/
